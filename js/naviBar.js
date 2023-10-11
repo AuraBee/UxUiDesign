0e01a4085204e6351a1e8b0450d9281561c158fe
Skybee <64372195+AuraBee@users.noreply.github.com>
2023-10-11 17:45:20 +0100

    Update naviBar.js
    review and edit.

--- js/naviBar.js
+++ js/naviBar.js
@@ -2,16 +2,16 @@
 
 // Function to create and render the navbar
 export function renderNavbar() {
-    // Get the container element where the navbar will be added
+    // Gets the container element where the navbar will be added
     const navbarContainer = document.getElementById('navbarContainer');
 
     // Creates the navbar element
     const navbar = document.createElement('nav');
     navbar.classList.add('navbar'); // Classe here added for styling
-    // Create an unordered list (ul) for the navbar links
+    // Creates the unordered list (ul) for the navbar links
     const navList = document.createElement('ul');
 
-    // Create individual list items (li) for the navbar links
+    // Creates individual list items (li) for the navbar links
     const homeNavItem = document.createElement('li');
     homeNavItem.textContent = 'Home'; // Set the text content
     const aboutNavItem = document.createElement('li');
@@ -19,15 +19,15 @@ export function renderNavbar() {
     const contactNavItem = document.createElement('li');
     contactNavItem.textContent = 'Contact';
 
-    // Append list items to the navbar
+    // Appends list items to the navbar
     navList.appendChild(homeNavItem);
     navList.appendChild(aboutNavItem);
     navList.appendChild(contactNavItem);
 
-    // Append the list to the navbar
+    // Appends the list to the navbar
     navbar.appendChild(navList);
 
-    // Append the navbar to the container
+    // Appends the navbar to the container
     navbarContainer.appendChild(navbar);
 }
 
